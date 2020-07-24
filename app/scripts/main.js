915e292d60a5877c32c64a5afed2353988ef1981
Darya Murauyova <dmurauyova@dxagency.com>
2020-07-24 09:30:25 -0400

    continue working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-console.log('\'Allo \'Allo!');
+console.log("'Allo 'Allo!");
 
 // Uncomment to enable Bootstrap tooltips
 // https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
@@ -7,3 +7,18 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+$(document).ready(function () {
+  $(".portfolio").on("click", function (e) {
+    if (jQuery(this).attr("aria-expanded") === "true") {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+  $(".portfolio-devices .btn").on("click", function () {
+    let target = $(this).attr("href");
+    console.log(target);
+    $(target).show().addClass("animated bounceOutLeft");
+  });
+});
